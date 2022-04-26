62f66837a1501b199dfb699948edc812861ff8c0
Shoyayeb <sa.fine@outlook.com>
2022-04-27 05:23:38 +0600

    sending password to db with hashing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 const User = require("./models/user.model");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 app.use(cors());
@@ -13,36 +14,43 @@ const port = process.env.PORT || 5000;
 mongoose.connect('mongodb://localhost:27017/authentication-server');
 
 // login
-app.post("/login", async (req, res) => {
+app.get("/login", async (req, res) => {
     // console.log(req.body);
-    const user = await User.findOne({ email: req.body.email, password: req.body.password });
+    const user = await User.findOne({
+        email: req.body.email
+    });
 
-    if (user) {
+    if (!user) {
+        return { status: 'error', error: 'Invalid login' }
+    }
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+    if (isPasswordValid) {
         const token = jwt.sign({
             name: user.name,
             email: user.email
-        }, process.env.SECRET)
-        return res.json({ status: 'ok', user: token })
+        }, process.env.JWTSECRET);
+
+        return res.json({ status: 'ok', user: token });
     } else {
         return res.json({ status: 'error', user: false })
     }
 });
 
 // register
-app.post("/register", async (req, res) => {
+app.get("/register", async (req, res) => {
     // console.log(req.body);
     try {
+        const newPassword = await bcrypt.hash(req.body.password, 10)
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: newPassword,
         })
         res.json({ status: 'ok' })
     } catch (err) {
         // console.log(err);
         res.json({ status: 'error', error: 'Duplicate email' })
     }
-    // res.json({ status: 'ok' })
 });
 
 app.listen(port, () => {
--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+
 const User = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    quote: { type: String }
+    password: { type: String, required: true }
 },
     { collection: 'user-data' }
 )
